43b6e054cc1176269264183c7fed7076a1375f21
Jeff Gonzalez <jeff@hypertheory.com>
2025-07-08 11:10:03 -0400

    got rid of the name formatter

--- week-1-instructor/ReferencesSolution/References.Api/Links/LinkController.cs
+++ week-1-instructor/ReferencesSolution/References.Api/Links/LinkController.cs
@@ -3,13 +3,13 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace References.Api.Links;
 
-public class LinkController(NameFormatter formatter) : ControllerBase
+public class LinkController() : ControllerBase
 {
     // some code here that will get called when a GET /links is sent to this server.
 
     [HttpGet("/links")]
     public async Task<ActionResult> GetAllLinksAsync(CancellationToken token)
     {
-        return Ok(formatter.FormatName("Bob", "Smith"));
+        return Ok();
     }
 }
--- week-1-instructor/ReferencesSolution/References.Api/NameFormatter.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace References.Api;
-
-public class NameFormatter
-{
-    public string FormatName(string firstName,  string lastName, char mi)
-    {
-        return $"{firstName} {lastName}";
-    }
-}
