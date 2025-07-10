3b8d013a76ad2004a05fcd2d0a49e315a54fae9c
Jeff Gonzalez <jeff@hypertheory.com>
2025-07-10 11:24:21 -0400

    jeff string calculator

---
+++ week-1-instructor/StringCalculatorThursdayMorning/StringCalculator/Calculator.cs
@@ -0,0 +1,28 @@
+﻿
+public class Calculator
+{
+    public int Add(string numbers)
+    {
+        List<char> delimeters = [',', '\n'];
+
+        if (numbers == "")
+        {
+            return 0;
+        }
+
+        if(numbers.StartsWith("//"))
+        {
+
+            var delimeter = numbers[2];
+            delimeters.Add(delimeter);
+            numbers = numbers.Substring(4);
+           
+        }
+ 
+        return numbers.Split(delimeters.ToArray()) //string[]
+             .Select(int.Parse) // int[]
+             .Sum();
+
+
+    }
+}
