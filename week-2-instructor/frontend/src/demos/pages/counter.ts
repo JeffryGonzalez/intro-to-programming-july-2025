2c88ae1d14c86b401c18ed3b7b7fe1e3a1cd6667
Jeff Gonzalez <jeff@hypertheory.com>
2025-07-14 15:21:25 -0400

    before break

--- week-2-instructor/frontend/src/demos/pages/counter.ts
+++ week-2-instructor/frontend/src/demos/pages/counter.ts
@@ -1,4 +1,9 @@
-import { Component, ChangeDetectionStrategy } from '@angular/core';
+import {
+  Component,
+  ChangeDetectionStrategy,
+  signal,
+  computed,
+} from '@angular/core';
 
 @Component({
   selector: 'app-demos-counter',
@@ -6,11 +11,35 @@ import { Component, ChangeDetectionStrategy } from '@angular/core';
   imports: [],
   template: `
     <div>
-      <button class="btn btn-warning btn-circle">-</button>
-      <span class="px-4">0</span>
-      <button class="btn btn-success btn-circle">+</button>
+      <button (click)="decrement()" class="btn btn-warning btn-circle">
+        -
+      </button>
+      <span class="px-4">{{ current() }}</span>
+      <button (click)="increment()" class="btn btn-success btn-circle">
+        +
+      </button>
+    </div>
+
+    <div>
+      @if (isEven()) {
+        <p>It is Even</p>
+      } @else {
+        <p>It is Odd!</p>
+      }
     </div>
   `,
   styles: ``,
 })
-export class Counter {}
+export class Counter {
+  current = signal(0);
+
+  isEven = computed(() => this.current() % 2 === 0);
+
+  decrement() {
+    this.current.update((c) => c - 1);
+  }
+
+  increment() {
+    this.current.update((c) => c + 1);
+  }
+}
